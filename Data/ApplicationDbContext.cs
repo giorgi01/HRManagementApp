a7e1e9dfe3335726c9d2ebb1fd875961cfeddd87
Giorgi Tamarashvili <giorgi.tamarashvili.1@btu.edu.ge>
2021-03-08 20:40:55 +0400

    add employee

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using HRManagementApp.Models;
 
 namespace HRManagementApp.Data
 {
@@ -12,5 +13,6 @@ namespace HRManagementApp.Data
             : base(options)
         {
         }
+        public DbSet<HRManagementApp.Models.Employee> Employee { get; set; }
     }
 }
---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRManagementApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public int PersonalID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
