4ea9cb8ffc16b04d1001a4598cbfde8ac018f8e9
Giorgi Tamarashvili <giorgi.tamarashvili.1@btu.edu.ge>
2021-03-08 19:49:31 +0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HRManagementApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
